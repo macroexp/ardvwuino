262352ad788cec4c5af7fcbf46da5d0faf7c0b7b
Mike Shields <mike@shieldsworld.com>
2016-11-03 00:11:26 -0400

    oo

--- libraries/Log/WireWrapper.h
+++ libraries/Log/WireWrapper.h
@@ -1,9 +1,9 @@
 class WireWrapper : public IWire {
 	public:
 		unsigned long write(const char *str){
-			Wire.write(str);
+			return Wire.write(str);
 		}
 		unsigned long write(int value){
-			Wire.write(value);
+			return Wire.write(value);
 		}
 };
